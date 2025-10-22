ad811d45f18cf3727f14c45047d651857fc2e9a3
Pratip Kayal <pratipkayal1@gmail.com>
2025-10-22 20:13:40 +0530

    commit

--- src/Bill.jsx
+++ src/Bill.jsx
@@ -48,17 +48,17 @@ const Bill = () => {
         </select>
       </div>
 
-      <div ref={componentRef} id="print-area" className="w-[4in] h-[5.5in] p-2 border-4 border-black flex flex-col justify-between">
+      <div ref={componentRef} id="print-area" className="w-[4in] h-[5.5in] p-2 border-2 border-black flex flex-col justify-between">
         {displayOption === "logo" ? (
           <>
-            <table className="w-full border-collapse border-4 border-black text-[22px] flex-grow">
+            <table className="w-full border-collapse border-2 border-black text-[22px] flex-grow">
               <tbody>
                 {["sortNo", "grade", "rollNo", "length", "width", "grossWt"].map((field, i) => (
                   <tr key={i}>
-                    <td className="border-4 border-black p-1 font-extrabold w-1/3 text-center">
+                    <td className="border-2 border-black p-1 font-extrabold w-1/3 text-center">
                       {field === "sortNo" ? "Sort No" : field === "grossWt" ? "Gross Wt." : field.charAt(0).toUpperCase() + field.slice(1)}
                     </td>
-                    <td className="border-4 border-black p-1 w-2/3">
+                    <td className="border-2 border-black p-1 w-2/3">
                       <input ref={(el) => (inputRefs.current[i] = el)} type="text" name={field} value={formData[field]} onChange={handleChange} onKeyDown={(e) => handleKeyDown(e, i)} className="w-full text-center outline-none uppercase font-bold text-3xl" />
                     </td>
                   </tr>
@@ -71,24 +71,24 @@ const Bill = () => {
           </>
         ) : (
           <>
-            <table className="w-full border-collapse border-4 border-black text-lg flex-grow">
+            <table className="w-full border-collapse border-1 border-black text-lg flex-grow">
               <tbody>
                 {["sortNo", "grade", "rollNo", "refNo", "length", "width", "grossWt"].map((field, i) => (
                   <tr key={i}>
-                    <td className="border-4 border-black p-1 font-extrabold w-1/3 text-center">
+                    <td className="border-2 border-black p-1 font-extrabold w-1/3 text-center">
                       {field === "sortNo" ? "Sort No" : field === "refNo" ? "Ref No" : field === "grossWt" ? "Gross Wt." : field.charAt(0).toUpperCase() + field.slice(1)}
                     </td>
-                    <td colSpan={2} className="border-4 border-black p-1 text-center">
+                    <td colSpan={2} className="border-2 border-black p-1 text-center">
                       <input ref={(el) => (barcodeInputRefs.current[i] = el)} type="text" name={field} value={barcodeData[field]} onChange={handleBarcodeChange} onKeyDown={(e) => handleKeyDown(e, i, true)} className="w-full text-center outline-none uppercase font-bold text-3xl" />
                     </td>
                   </tr>
                 ))}
                 <tr>
-                  <td className="border-4 border-black p-1 font-extrabold w-1/3 text-center">No of Pcs</td>
-                  <td className="border-4 border-black p-1">
+                  <td className="border-2 border-black p-1 font-extrabold w-1/3 text-center">No of Pcs</td>
+                  <td className="border-2 border-black p-1">
                     <input ref={(el) => (barcodeInputRefs.current[7] = el)} type="text" name="noOfPcs1" value={barcodeData.noOfPcs1} onChange={handleBarcodeChange} onKeyDown={(e) => handleKeyDown(e, 7, true)} className="w-full text-center outline-none uppercase font-bold text-3xl" />
                   </td>
-                  <td className="border-4 border-black p-1">
+                  <td className="border-2 border-black p-1">
                     <input ref={(el) => (barcodeInputRefs.current[8] = el)} type="text" name="noOfPcs2" value={barcodeData.noOfPcs2} onChange={handleBarcodeChange} onKeyDown={(e) => handleKeyDown(e, 8, true)} className="w-full text-center outline-none uppercase font-bold text-3xl" />
                   </td>
                 </tr>
